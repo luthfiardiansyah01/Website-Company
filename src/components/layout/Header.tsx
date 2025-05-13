640b140a6e9a06757cc31970151b2d93989b710e
luthfiardiansyah01 <luthfiardiansyah2001@gmail.com>
2025-05-13 21:46:26 +0700

    Deploy application to Netlify

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
       <div className="container mx-auto px-4 md:px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <img src="/startup-transparan-2.png" alt="Startup Icon" className="h-20 w-20 animate-pulse"/>
+            <img src="/startup-transparan-2.png" alt="Startup Icon" className="h-15 w-20 animate-pulse"/>
             <span className="text-2xl font-bold bg-gradient-to-r from-[#00f0ff] to-[#ff00c8] text-transparent bg-clip-text">
               MoedaTrace
             </span>
